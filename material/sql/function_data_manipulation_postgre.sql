bbed533e537931cd4e5c6c159b5eea8b315a6b70
Aprilia Titisari <tiatitisari@gmail.com>
2023-07-04 14:17:32 -0500

    manipulate

--- material/sql/function_data_manipulation_postgre.sql
+++ material/sql/function_data_manipulation_postgre.sql
@@ -220,3 +220,218 @@ SELECT
 ---Extracting and transforming date/time data--- 
 #####################
 -- DATE_PART(), DATE_TRUNC() , EXTRACT() 
+-- EXTRACT(field FROM source), DATE_PART('field',source)
+-- DATE_TRUNC(month, year, day) -- timestamp
+
+SELECT 
+  -- Extract day of week from rental_date
+  EXTRACT(dow FROM rental_date) AS dayofweek 
+FROM rental 
+LIMIT 100;
+
+-- Extract day of week from rental_date
+SELECT 
+  EXTRACT(dow FROM rental_date) AS dayofweek, 
+  -- Count the number of rentals
+  COUNT(*) as rentals 
+FROM rental 
+GROUP BY 1;
+
+-- Truncate rental_date by year
+SELECT DATE_TRUNC('year', rental_date) AS rental_year
+FROM rental;
+
+-- Truncate rental_date by month
+SELECT DATE_TRUNC('month', rental_date) AS rental_month
+FROM rental;
+
+SELECT 
+  DATE_TRUNC('day', rental_date) AS rental_day,
+  -- Count total number of rentals 
+  COUNT(*) AS rentals 
+FROM rental
+GROUP BY 1;
+
+SELECT 
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(dow FROM rental_date) AS dayofweek,
+  AGE(return_date, rental_date) AS rental_days
+FROM rental AS r 
+WHERE 
+  -- Use an INTERVAL for the upper bound of the rental_date 
+  rental_date BETWEEN CAST('2005-05-01' AS DATE)
+   AND CAST('2005-05-01' AS DATE) + INTERVAL '90 day';
+
+SELECT 
+  c.first_name || ' ' || c.last_name AS customer_name,
+  f.title,
+  r.rental_date,
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(dow FROM r.rental_date) AS dayofweek,
+  AGE(r.return_date, r.rental_date) AS rental_days,
+  -- Use DATE_TRUNC to get days from the AGE function
+  CASE WHEN DATE_TRUNC('day', AGE(r.return_date, r.rental_date)) > 
+  -- Calculate number of d
+    f.rental_duration * INTERVAL '1' day 
+  THEN TRUE 
+  ELSE FALSE END AS past_due 
+FROM 
+  film AS f 
+  INNER JOIN inventory AS i 
+  	ON f.film_id = i.film_id 
+  INNER JOIN rental AS r 
+  	ON i.inventory_id = r.inventory_id 
+  INNER JOIN customer AS c 
+  	ON c.customer_id = r.customer_id 
+WHERE 
+  -- Use an INTERVAL for the upper bound of the rental_date 
+  r.rental_date BETWEEN CAST('2005-05-01' AS DATE) 
+  AND CAST('2005-05-01' AS DATE) + INTERVAL '90 day';
+
+#####################
+-- Reformatting string and character data 
+#####################
+-- reformatting string adn character data 
+-- parsing string and character data 
+-- determine string length and character position 
+-- truncating and padding string data 
+-- Concatenate the first_name and last_name and email 
+
+SELECT first_name ||' '|| last_name ||' <'|| email || '>' AS full_email 
+FROM customer;
+
+-- Concatenate the first_name and last_name and email
+SELECT CONCAT(first_name,' ', last_name,' <', email,'>') AS full_email 
+FROM customer;
+
+SELECT 
+  -- Concatenate the category name to coverted to uppercase
+  -- to the film title converted to title case
+  UPPER(name)  || ': ' || INITCAP(title) AS film_category, 
+  -- Convert the description column to lowercase
+  LOWER(description) AS description
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
+
+SELECT 
+  -- Replace whitespace in the film title with an underscore
+  REPLACE(title,' ','_') AS title
+FROM film; 
+
+SELECT 
+  -- Select the title and description columns
+  title,
+  description,
+  -- Determine the length of the description column
+  CHAR_LENGTH(description) AS desc_len,
+  LENGTH(description)
+FROM film;
+
+SELECT 
+  -- Select the first 50 characters of description
+  LEFT(description, 50) AS short_desc
+FROM 
+  film AS f; 
+
+SELECT 
+  -- Select only the street name from the address table
+  SUBSTRING(address FROM POSITION(' ' IN address)+1 FOR CHAR_LENGTH(address))
+  FROM
+  address;
+
+SELECT
+  -- Extract the characters to the left of the '@'
+  SUBSTRING(email FROM 0 FOR POSITION ('@' IN email)) AS username,
+  -- Extract the characters to the right of the '@'
+  SUBSTRING(email FROM POSITION('@' IN email)+1 FOR CHAR_LENGTH(email)) AS domain
+FROM customer;
+
+
+-- Concatenate the first_name and last_name 
+SELECT 
+	first_name || LPAD(last_name, LENGTH(last_name)+1) AS full_name
+FROM customer; 
+
+-- Concatenate the first_name and last_name 
+SELECT 
+	RPAD(first_name, LENGTH(first_name)+1) 
+    || RPAD(last_name, LENGTH(last_name)+2, ' <') 
+    || RPAD(email, LENGTH(email)+1, '>') AS full_email
+FROM customer; 
+
+-- Concatenate the uppercase category name and film title
+SELECT 
+  CONCAT(UPPER(name), ': ', title) AS film_category, 
+  -- Truncate the description remove trailing whitespace
+  TRIM(LEFT(description, 50)) AS film_desc
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
+
+    SELECT 
+  UPPER(c.name) || ': ' || f.title AS film_category, 
+  -- -- Truncate the description without cutting off a word
+  LEFT(description, 50 - 
+  --   -- Subtract the position of the first whitespace character
+    POSITION(
+      ' ' IN REVERSE(LEFT(description, 50))
+    )
+  ) 
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
+
+    -- STRPOS
+
+#####################
+-- Introduction to full-text search 
+#####################
+-- Full Text search : provides a means for performing natural language queries of text data in your database: 
+-- stemming, spelling mistake, ranking 
+-- Extending PostgreSQL
+-- Improving full text search with extensions 
+
+-- Select all columns
+SELECT *
+FROM film
+-- Select only records that begin with the word 'GOLD'
+WHERE title LIKE 'GOLD%';
+
+SELECT *
+FROM film
+-- Select only records that end with the word 'GOLD'
+WHERE title LIKE '%GOLD';
+
+SELECT *
+FROM film
+-- Select only records that contain the word 'GOLD'
+WHERE title LIKE '%GOLD%';
+
+-- Select the film description as a tsvector
+SELECT to_tsvector(description)
+FROM film;
+
+-- Select the title and description
+SELECT title, description
+FROM film
+-- Convert the title to a tsvector and match it against the tsquery 
+WHERE to_tsvector(title) @@ to_tsquery('elf');
+
+CREATE TYPE dayofweek AS ENUM('Monday', 'Tuesday');
+
+-- data type E >> Enum 
+CREATE FUNCTION squared(i integer) RETURNS integer AS $$ -- double dollar sign show that the function will be using sql 
+        BEGIN 
+            RETURN i*i: 
+        END; 
+    $$ LANGUAGE plpgsql; 
