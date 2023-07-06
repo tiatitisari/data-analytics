c718c3e68d6bfe618ac4cf55888be33a19e6f5d4
Aprilia Titisari <tiatitisari@gmail.com>
2023-07-06 14:09:23 -0500

    Add database

--- material/sql/exploratory_data_analysis.sql
+++ material/sql/exploratory_data_analysis.sql
@@ -3,7 +3,7 @@
 -- and alias as missing
 SELECT count(*) - COUNT(ticker) AS missing
   FROM fortune500;
--- COALESCE -- between column 
+== COALESCE == between column 
 -- Use coalesce
 SELECT COALESCE(industry, sector, 'Unknown') AS industry2,
        -- Don't forget to count!
@@ -33,3 +33,41 @@ SELECT COALESCE(industry, sector, 'Unknown') AS industry2,
  -- For clarity, order by rank
  ORDER BY rank; 
 
+== CASTING ==
+-- Select the original value
+SELECT profits_change, 
+	   -- Cast profits_change
+       CAST(profits_change AS INT) AS profits_change_int
+  FROM fortune500;
+
+-- Divide 10 by 3
+SELECT 10/3, 
+       -- Cast 10 as numeric and divide by 3
+       10::numeric/3;
+
+SELECT '3.2'::numeric,
+       '-123'::numeric,
+       '1e3'::numeric,
+       '1e-3'::numeric,
+       '02314'::numeric,
+       '0002'::numeric;
+
+-- Select the count of each value of revenues_change
+SELECT revenues_change, COUNT(*)
+  FROM fortune500
+ GROUP BY 1 
+ -- order by the values of revenues_change
+ ORDER BY 1;
+
+ -- Select the count of each revenues_change integer value
+SELECT revenues_change::integer, COUNT(*)
+  FROM fortune500
+GROUP BY revenues_change::integer
+ -- order by the values of revenues_change
+ ORDER BY 1;
+ 
+ -- Count rows 
+SELECT COUNT(*)
+  FROM fortune500
+ -- Where...
+ WHERE revenues_change >0;
